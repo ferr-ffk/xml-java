d8fa6aed2b553f67f5b7f861a08237dc33fdbb65
Fernando Freitas <freitax.li@gmail.com>
2024-09-13 23:16:25 -0300

    feat: criar código de exemplo para salvar objeto em XML

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import model.Pessoa;
+import util.XMLUtil;
+
 public class Main {
+    private static final Pessoa pessoa = new Pessoa("Steffeson", "Sousa", "steffe@gmail.com", "Avenida do Cursino", "1294");
+
     public static void main(String[] args) {
-        System.out.println("oie");
+        XMLUtil.salvarObjetoEmArquivo(pessoa);
     }
 }
