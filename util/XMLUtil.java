76ade60667b6fea0ffba2a5cb119393c476ad6a5
Fernando Freitas <freitax.li@gmail.com>
2024-09-14 11:29:17 -0300

    remove: remover import desnecessário

--- util/XMLUtil.java
+++ util/XMLUtil.java
@@ -20,8 +20,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import model.Pessoa;
-
 public class XMLUtil {
     private static String caminhoArquivo = "tmp.xml";
 
